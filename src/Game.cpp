f79d845360634126d5beac552b5c770dcbbda03a
Duy Vu <hdvu23@apcs.fitus.edu.vn>
2024-12-28 13:56:00 +0700

    add map

--- src/Game.cpp
+++ src/Game.cpp
@@ -76,7 +76,7 @@ void Game::loadTextures() {
 	//Background
 	TextureHolder::getInstance().load(Textures::ID::BackGround1, "res/Background/bg_kitchen.png");
 	TextureHolder::getInstance().load(Textures::ID::BackGround2, "res/Background/bg_outside.png");
-	TextureHolder::getInstance().load(Textures::ID::BackGround3, "res/Background/backround3.png");
+	TextureHolder::getInstance().load(Textures::ID::BackGround3, "res/Background/backround3.jpg");
 	
 	TextureHolder::getInstance().load(Textures::ID::TitleScreen, "res/Background/title.png");
 	TextureHolder::getInstance().load(Textures::ID::ChooseCharScreen, "res/Background/ChooseCharScreen.png");
